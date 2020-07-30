efd2a63295760598b598d4b31bec6417548c26db
Alexander Fieger <alexander.fieger@outlook.com>
2020-07-30 14:01:57 +0200

    :lipstick: fixed typo

--- test/murky_web/live/page_live_test.exs
+++ test/murky_web/live/page_live_test.exs
@@ -4,27 +4,29 @@ defmodule MurkyWeb.PageLiveTest do
   import Phoenix.LiveViewTest
 
   setup_all do
-    # make shure the STORAGE_PATH exists
+    # make sure the STORAGE_PATH exists
     dir = Murky.Data.get_storage_path()
-    
+
     if !File.exists?(dir) do
       File.mkdir!(dir)
 
-      on_exit fn -> 
+      on_exit(fn ->
         File.rmdir!(Murky.Data.get_storage_path())
-      end
+      end)
     end
+
     :ok
   end
 
-  test "make shure index page is shown", %{conn: conn} do
+  test "make sure index page is shown", %{conn: conn} do
     {:ok, _page_live, disconnected_html} = live(conn, "/")
 
     {:ok, document} = Floki.parse_document(disconnected_html)
-    
-    index_page_found = document
-    |> Floki.find(".index-page")
-    |> Enum.count
+
+    index_page_found =
+      document
+      |> Floki.find(".index-page")
+      |> Enum.count()
 
     assert index_page_found == 1
   end
