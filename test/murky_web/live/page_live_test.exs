1289100dbdd8ce7e7abdcac28dbf5644746e2d62
Alexander Fieger <alexander.fieger@outlook.com>
2020-07-30 17:57:26 +0200

    :white_check_mark: another test for index page added

--- test/murky_web/live/page_live_test.exs
+++ test/murky_web/live/page_live_test.exs
@@ -2,18 +2,15 @@ defmodule MurkyWeb.PageLiveTest do
   use MurkyWeb.ConnCase
 
   import Phoenix.LiveViewTest
+  alias Murky.Data
 
-  setup_all do
+  setup do
     # make sure the STORAGE_PATH exists
-    dir = Murky.Data.get_storage_path()
+    Data.get_storage_path() |> File.mkdir!()
 
-    if !File.exists?(dir) do
-      File.mkdir!(dir)
-
-      on_exit(fn ->
-        File.rmdir!(Murky.Data.get_storage_path())
-      end)
-    end
+    on_exit(fn ->
+      File.rm_rf!(Murky.Data.get_storage_path())
+    end)
 
     :ok
   end
@@ -29,5 +26,18 @@ defmodule MurkyWeb.PageLiveTest do
       |> Enum.count()
 
     assert index_page_found == 1
+  end
+
+  test "existing files are listed in the index page", %{conn: conn} do
+    Data.create_file("first_file")
+    Data.create_file("second_file")
+
+    {:ok, page_live, _disconnected_html} = live(conn, "/")
+    html = render(page_live)
+    {:ok, document} = Floki.parse_document(html)
+
+    c = document |> Floki.find(".index-list__item") |> Enum.count()
+
+    assert c == 2
   end
 end
