80ec4d345e3460622d149dcdc3d7eb532be46022
Alexander Fieger <alexander.fieger@outlook.com>
2020-07-31 11:42:06 +0200

    :white_check_mark: added more tests for view page

--- lib/murky/data.ex
+++ lib/murky/data.ex
@@ -1,25 +1,25 @@
 defmodule Murky.Data do
+  def get_md(file_name) do
+    File.read!(get_storage_path() <> "/" <> file_name)
+    |> Earmark.as_html!(%Earmark.Options{code_class_prefix: "lang- language-"})
+  end
 
-    def get_md(file_name) do
-        File.read!("wiki/" <> file_name)
-        |> Earmark.as_html!(%Earmark.Options{code_class_prefix: "lang- language-"})
-    end
+  def get_files() do
+    get_storage_path()
+    |> File.ls!()
 
-    def get_files() do
-        get_storage_path()
-        |> File.ls!
-        # File.ls!(get_storage_path())
-    end
+    # File.ls!(get_storage_path())
+  end
 
-    def create_file(filename) do
-        File.touch!(get_storage_path() <> "/" <> filename <> ".md")
-    end
+  def create_file(filename) do
+    File.touch!(get_storage_path() <> "/" <> filename <> ".md")
+  end
 
-    def save_file(filename, content) do
-        File.write!(get_storage_path() <> "/" <> filename, content)
-    end
+  def save_file(filename, content) do
+    File.write!(get_storage_path() <> "/" <> filename, content)
+  end
 
-    def get_storage_path() do
-        System.fetch_env!("STORAGE_PATH")
-    end
+  def get_storage_path() do
+    System.fetch_env!("STORAGE_PATH")
+  end
 end
---
+++ test/murky_web/live/view_live_test.exs
@@ -0,0 +1,57 @@
+defmodule MurkyWeb.ViewLiveTest do
+  use MurkyWeb.ConnCase
+  import Phoenix.LiveViewTest
+  alias Murky.Data
+
+  setup do
+    # make sure the STORAGE_PATH exists
+    Data.get_storage_path() |> File.mkdir!()
+
+    on_exit(fn ->
+      File.rm_rf!(Murky.Data.get_storage_path())
+    end)
+
+    :ok
+  end
+
+  test "make sure the markdwon view page is shown", %{conn: conn} do
+    Data.create_file("first_file")
+    Data.save_file("first_file.md", "# Das ist ein Titel")
+
+    {:ok, view, _disconnected_html} =
+      live(conn, Routes.live_path(conn, MurkyWeb.ViewLive, file: "first_file.md"))
+
+    assert view
+           |> render()
+           |> Floki.parse_document!()
+           |> Floki.find(".markdown_view")
+           |> Enum.count() == 1
+  end
+
+  test "shows the content of the file", %{conn: conn} do
+    Data.create_file("first_file")
+    Data.save_file("first_file.md", "# Das ist ein Titel")
+
+    {:ok, view, _disconnected_html} =
+      live(conn, Routes.live_path(conn, MurkyWeb.ViewLive, file: "first_file.md"))
+
+    assert view
+           |> render()
+           |> Floki.parse_document!()
+           |> Floki.find("div[class='markdown'] h1")
+           |> Floki.text() =~ "Das ist ein Titel"
+  end
+
+  test "click on edit redirects to the edit page", %{conn: conn} do
+    Data.create_file("first_file")
+
+    {:ok, view, _disconnected_html} =
+      live(conn, Routes.live_path(conn, MurkyWeb.ViewLive, file: "first_file.md"))
+
+    view
+    |> element(".markdown_view__header a")
+    |> render_click()
+
+    assert_redirect(view, Routes.live_path(conn, MurkyWeb.EditLive, file: "first_file.md"))
+  end
+end
