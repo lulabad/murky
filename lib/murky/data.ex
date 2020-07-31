e7d7eff0a068630ae4edc1211fa11e414571ed62
Alexander Fieger <alexander.fieger@outlook.com>
2020-07-31 19:54:10 +0200

    :hammer: use of Path module and added specs

--- lib/murky/data.ex
+++ lib/murky/data.ex
@@ -1,22 +1,36 @@
 defmodule Murky.Data do
-  def get_md(file_name) do
-    File.read!(get_storage_path() <> "/" <> file_name)
+  def get_md(filename) do
+    get_storage_path()
+    |> Path.join(filename)
+    |> File.read!()
     |> Earmark.as_html!(%Earmark.Options{code_class_prefix: "lang- language-"})
   end
 
+  @spec get_files() :: Enum.t()
   def get_files() do
     get_storage_path()
     |> File.ls!()
   end
 
+  @spec create_file(String.t()) :: :ok
   def create_file(filename) do
-    File.touch!(get_storage_path() <> "/" <> filename <> ".md")
+    get_storage_path()
+    |> Path.join(filename <> ".md")
+    |> File.touch!()
+
+    :ok
   end
 
+  @spec save_file(String.t(), String.t()) :: :ok
   def save_file(filename, content) do
-    File.write!(get_storage_path() <> "/" <> filename, content)
+    get_storage_path()
+    |> Path.join(filename)
+    |> File.write!(content)
+
+    :ok
   end
 
+  @spec get_storage_path() :: String.t()
   def get_storage_path() do
     System.fetch_env!("STORAGE_PATH")
   end
