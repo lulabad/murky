9ad943b5623ab37a252fb9de18e2a7da73c11a52
Alexander Fieger <alexander.fieger@outlook.com>
2021-01-22 19:35:01 +0100

    :arrow_up: Bump mix dependencies to latest (#22)
    * :arrow_up: Bump mix dependencies to latest
    
    * :arrow_up: Bump phoenix_live_view from 0.14.0 to 0.15.3

--- mix.exs
+++ mix.exs
@@ -34,11 +34,11 @@ defmodule Murky.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.1"},
-      {:phoenix_live_view, "~> 0.14.0"},
+      {:phoenix_live_view, "~> 0.15.0"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.7"},
+      {:phoenix_live_dashboard, "~> 0.4.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
