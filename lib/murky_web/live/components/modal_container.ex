ea5ec0248acc2df7102d34b4bd28d16c40ca4184
Alexander Fieger <alexander.fieger@outlook.com>
2021-01-22 19:31:37 +0100

    :arrow_up: Bump phoenix_live_view from 0.14.0 to 0.15.3

--- lib/murky_web/live/components/modal_container.ex
+++ lib/murky_web/live/components/modal_container.ex
@@ -5,7 +5,7 @@ defmodule MurkyWeb.Component.ModalContainer do
     ~L"""
     <div class="absolute flex inset-0 z-50 bg-gray-700 bg-opacity-25 justify-center items-center" phx-capture-click="<%= @close %>">
       <div class="relative bg-primary-200 p-8 rounded-lg shadow-md border-primary-200 border">
-        <%= @inner_content.([]) %>
+        <%= render_block(@inner_block) %>
       </div>
     </div>
     """
